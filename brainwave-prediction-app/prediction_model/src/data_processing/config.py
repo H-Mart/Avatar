1e4a5a48cb9547303e6d07c46ac4d7c1c2ccd3c5
henry <ghosttaco1@gmail.com>
2024-03-18 22:59:54 -0500

    trying a way to update the config

--- brainwave-prediction-app/prediction_model/src/data_processing/config.py
+++ brainwave-prediction-app/prediction_model/src/data_processing/config.py
@@ -1,30 +1,83 @@
 import json
 from pathlib import Path
 import logging
+from os import PathLike
+from typing import Final
+
+
+def refresh_config():
+    config_path = Path(__file__).parent / Path('training_config.json')
+    config_obj = json.load(config_path.open())
+
+    globals()['data_paths'] = config_obj['data_paths']
+    globals()['base_data_path'] = Path(data_paths['base'])
+    globals()['extract_dir_path'] = base_data_path / Path(data_paths['raw'])
+    globals()['processed_dir_path'] = base_data_path / Path(data_paths['processed'])
+    globals()['filtered_dir_path'] = base_data_path / Path(data_paths['filtered'])
+    globals()['deltalake_table_path'] = base_data_path / Path(data_paths['deltalake_table'])
+    globals()['set_aside_path'] = base_data_path / Path(data_paths['set_aside'])
+    globals()['processed_minus_set_aside'] = base_data_path / Path(data_paths['processed_minus_set_aside'])
+
+    globals()['spark_config'] = config_obj['spark']
+    globals()['spark_executor_memory'] = spark_config['executor_memory']
+    globals()['spark_driver_memory'] = spark_config['driver_memory']
+    globals()['spark_timeout'] = spark_config['timeout']
+
+    globals()['model_save_dir_path'] = Final[Path](config_obj['models']['save_directory'])
+    globals()['model_stats_path'] = Final[Path](config_obj['models']['stats'])
+
+    globals()['headset_streaming_host'] = config_obj['headset_streaming']['host']
+    globals()['headset_streaming_port'] = config_obj['headset_streaming']['port']
+    globals()['brainflow_batch_size'] = config_obj['headset_streaming']['batch_size']
+
+    globals()['use_intelex'] = config_obj['training']['use_intelex']
+
+
+def save_config_and_refresh(f):
+    f()
+    with config_path.open('w') as f:
+        json.dump(config_obj, f)
+    refresh_config()
+
+
+@save_config_and_refresh
+def set_base_data_path(path: PathLike):
+    config_obj['data_paths']['base'] = path
+
+
+@save_config_and_refresh
+def set_model_save_dir_path(path: PathLike):
+    config_obj['models']['save_directory'] = path
+
+
+@save_config_and_refresh
+def set_model_stats_path(path: PathLike):
+    config_obj['models']['stats'] = path
+
 
 config_path = Path(__file__).parent / Path('training_config.json')
 config_obj = json.load(config_path.open())
 
-data_paths: dict = config_obj['data_paths']
-base_data_path = Path(data_paths['base'])
-extract_dir_path = base_data_path / Path(data_paths['raw'])
-processed_dir_path = base_data_path / Path(data_paths['processed'])
-filtered_dir_path = base_data_path / Path(data_paths['filtered'])
-deltalake_table_path = base_data_path / Path(data_paths['deltalake_table'])
-set_aside_path = base_data_path / Path(data_paths['set_aside'])
-processed_minus_set_aside = base_data_path / Path(data_paths['processed_minus_set_aside'])
+data_paths: Final[dict] = config_obj['data_paths']
+base_data_path: Final[Path] = Path(data_paths['base'])
+extract_dir_path: Final[Path] = base_data_path / Path(data_paths['raw'])
+processed_dir_path: Final[Path] = base_data_path / Path(data_paths['processed'])
+filtered_dir_path: Final[Path] = base_data_path / Path(data_paths['filtered'])
+deltalake_table_path: Final[Path] = base_data_path / Path(data_paths['deltalake_table'])
+set_aside_path: Final[Path] = base_data_path / Path(data_paths['set_aside'])
+processed_minus_set_aside: Final[Path] = base_data_path / Path(data_paths['processed_minus_set_aside'])
 
-spark_config: dict = config_obj['spark']
-spark_executor_memory: str = spark_config['executor_memory']
-spark_driver_memory: str = spark_config['driver_memory']
-spark_timeout: str = spark_config['timeout']
+spark_config: Final[dict] = config_obj['spark']
+spark_executor_memory: Final[str] = spark_config['executor_memory']
+spark_driver_memory: Final[str] = spark_config['driver_memory']
+spark_timeout: Final[str] = spark_config['timeout']
 
-model_save_dir_path = Path(config_obj['models']['save_directory'])
-model_stats_path = Path(config_obj['models']['stats'])
+model_save_dir_path: Final[Path] = Path(config_obj['models']['save_directory'])
+model_stats_path: Final[Path] = Path(config_obj['models']['stats'])
 
-headset_streaming_host: str = config_obj['headset_streaming']['host']
-headset_streaming_port: int = config_obj['headset_streaming']['port']
-brainflow_batch_size: int = config_obj['headset_streaming']['batch_size']
+headset_streaming_host: Final[str] = config_obj['headset_streaming']['host']
+headset_streaming_port: Final[int] = config_obj['headset_streaming']['port']
+brainflow_batch_size: Final[int] = config_obj['headset_streaming']['batch_size']
 
 use_intelex: bool = config_obj['training']['use_intelex']
 
