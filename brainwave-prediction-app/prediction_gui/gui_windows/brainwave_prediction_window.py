9511f37fe11616b32d1720c23e016a83ad9ce315
henry <ghosttaco1@gmail.com>
2024-04-03 00:13:52 -0500

    updated gui to work with drone class made "read my mind" button not freeze the whole ui updated log textboxes to use the multiline and scroll at that bottom

--- brainwave-prediction-app/prediction_gui/gui_windows/brainwave_prediction_window.py
+++ brainwave-prediction-app/prediction_gui/gui_windows/brainwave_prediction_window.py
@@ -1,20 +1,20 @@
 import PySimpleGUI as sg
+from threading import Event
 
 from .bci_gui_tab import BCIGuiTab
 
-from ..client.drone import execute_drone_action
+from ..client.drone import Drone
 from ..client.bci_connection import BCIConnection
-
-get_drone_action_testing = execute_drone_action
 
 
 # changed script design to class object for variable retention in tabs
 class BrainwaveTab(BCIGuiTab):
-    def __init__(self, name: str = 'Brainwave Reading', image_dir: str = 'images'):
+    def __init__(self, drone: Drone, name: str = 'Brainwave Reading', image_dir: str = 'images'):
         self.name = name
         self.image_dir = image_dir
 
         self.bci_connection = BCIConnection()
+        self.drone = drone
 
         self.flight_log = []  # array to hold flight log info
         self.predictions_log = []  # array to hold info for table
@@ -22,6 +22,7 @@ class BrainwaveTab(BCIGuiTab):
         self.predictions_headings = ['Predictions Count', 'Server Predictions', 'Prediction Label']
         self.response_headings = ['Count', 'Label']
 
+        self.reading_in_progress = Event()
         self.count = 0
         self.prediction_label = None
 
@@ -44,10 +45,10 @@ class BrainwaveTab(BCIGuiTab):
             [sg.Radio('Manual Control', 'pilot', default=True, size=(-20, 1)),
              sg.Radio('Autopilot', 'pilot', size=(12, 1))],
             [self.read_my_mind_button],
-            [sg.Text("The model says ...")],
+            [sg.Text('The model says ...')],
             [sg.Table(values=[], headings=self.response_headings, auto_size_columns=False, def_col_width=15,
                       justification='center',
-                      num_rows=1, key=self.key('-SERVER_TABLE-'), row_height=25, tooltip="Server Response Table",
+                      num_rows=1, key=self.key('-SERVER_TABLE-'), row_height=25, tooltip='Server Response Table',
                       hide_vertical_scroll=True)],
             [self.not_what_i_was_thinking_button,
              self.execute_button, sg.Push()],
@@ -57,12 +58,13 @@ class BrainwaveTab(BCIGuiTab):
 
         bottom_left = [
             [sg.Text('Flight Log')],
-            [sg.Listbox(values=self.flight_log, size=(30, 6), key=self.key('LOG'))],
+            [sg.Multiline(auto_refresh=True, autoscroll=True, size=(30, 6), key=self.key('LOG'))],
         ]
 
         bottom_right = [
             [sg.Text('Console Log')],
-            [sg.Output(s=(45, 10))]
+            [sg.Multiline(echo_stdout_stderr=True, reroute_stdout=True, reroute_stderr=True, autoscroll=True,
+                          s=(45, 10))]
         ]
 
         brainwave_prediction_layout = [
@@ -76,8 +78,7 @@ class BrainwaveTab(BCIGuiTab):
                 key=self.key('-TABLE-'),
                 row_height=35,
                 tooltip='Predictions Table'
-            )
-             ],
+            )],
 
             [sg.Column(bottom_left), sg.Push(),
              sg.Column(bottom_right)],
@@ -93,38 +94,71 @@ class BrainwaveTab(BCIGuiTab):
         server_record = [[count, prediction_label]]
         window[self.key('-SERVER_TABLE-')].update(values=server_record)
 
-    def add_to_flight_log(self, window, text):
-        self.flight_log.insert(0, text)
-        window[self.key('LOG')].update(values=self.flight_log)
+    def add_to_flight_log(self, window, text: str):
+        if text:
+            self.flight_log.append(text)
+        else:
+            self.flight_log.append('None')
+        window[self.key('LOG')].update(value='\n'.join(self.flight_log))
 
     def add_to_predictions_log(self, window, prediction_count, server_predictions, prediction_label):
         prediction_record = [prediction_count, server_predictions, prediction_label]
         self.predictions_log.append(prediction_record)
         window[self.key('-TABLE-')].update(values=self.predictions_log)
 
+    def handle_read_my_mind_button(self, window):
+        if self.reading_in_progress.is_set():
+            return
+        self.reading_in_progress.set()
+        window.start_thread(lambda: self._process_headset_nonblocking(window), 'reading_done')
+
+    def handle_not_what_i_was_thinking_button(self, window):
+        if self.reading_in_progress.is_set():
+            return
+        self.add_to_predictions_log(window, 'manual', 'predict',
+                                    f'{self.prediction_label}')
+        self.add_to_flight_log(window, window[self.key('-drone_input-')].get())
+
+    def handle_execute_button(self, window):
+        if self.reading_in_progress.is_set():
+            return
+        self.add_to_flight_log(window, self.prediction_label)
+        self.drone.send_action(self.prediction_label)
+        self.add_to_flight_log(window, 'done')
+        self.add_to_predictions_log(window, len(self.predictions_log) + 1, self.count, self.prediction_label)
+
+    def handle_connect_button(self, window):
+        self.add_to_flight_log(window, 'Connect button pressed')
+        self.drone.send_action('connect')
+        self.add_to_flight_log(window, 'Done.')
+
+    def handle_keep_alive_button(self, window):
+        self.drone.send_action('keep alive')
+
+    def _process_headset_nonblocking(self, window):
+        self.set_server_table(window, self.count, 'reading...')
+        board_data_return = self.bci_connection.read_and_transmit_data_from_board()
+
+        if 'error' in board_data_return:
+            self.set_server_table(window, 'err', board_data_return['error'])
+        else:
+            self.prediction_label = board_data_return['prediction_label']
+            count = board_data_return['prediction_count']
+            self.set_server_table(window, count, self.prediction_label)
+
+        self.reading_in_progress.clear()
+
     def handle_event(self, window, event, values):
-
-        if event == self.read_my_mind_button.key:
-            prediction_response = self.bci_connection.read_and_transmit_data_from_board()
-            self.count = prediction_response['prediction_count']
-            self.prediction_label = prediction_response['prediction_label']
-            self.set_server_table(window, self.count, self.prediction_label)
-
-        elif event == self.not_what_i_was_thinking_button.key:
-            # get_drone_action_testing(values['-drone_input-'])
-            self.add_to_predictions_log(window, "manual", "predict",
-                                        f"{self.prediction_label}")
-
-        elif event == self.execute_button.key:
-            self.add_to_flight_log(window, self.prediction_label)
-            get_drone_action_testing(self.prediction_label)
-            self.add_to_flight_log(window, "done")
-            self.add_to_predictions_log(window, len(self.predictions_log) + 1, self.count, self.prediction_label)
-
-        elif event == self.connect_button.key:
-            self.add_to_flight_log(window, "Connect button pressed")
-            get_drone_action_testing('connect')
-            self.add_to_flight_log(window, "Done.")
-
-        elif event == self.keep_alive_button.key:
-            get_drone_action_testing('keep alive')
+        match event:
+            case self.read_my_mind_button.key:
+                self.handle_read_my_mind_button(window)
+            case self.not_what_i_was_thinking_button.key:
+                self.handle_not_what_i_was_thinking_button(window)
+            case self.execute_button.key:
+                self.handle_execute_button(window)
+            case self.connect_button.key:
+                self.handle_connect_button(window)
+            case self.keep_alive_button.key:
+                self.handle_keep_alive_button(window)
+            case _:
+                print(f'Event {event} not handled')
--- brainwave-prediction-app/prediction_gui/gui_windows/manual_drone_control_window.py
+++ brainwave-prediction-app/prediction_gui/gui_windows/manual_drone_control_window.py
@@ -3,13 +3,14 @@ from .bci_gui_tab import BCIGuiTab
 from pathlib import Path
 from dataclasses import dataclass
 
+from ..client.drone import Drone
+
 
 # changes method layout to class design for retention of data
 class DroneControlTab(BCIGuiTab):
-    def __init__(self, get_drone_action, name='Manual Drone Control', image_dir: str = 'images'):
+    def __init__(self, drone: Drone, name='Manual Drone Control', image_dir: str = 'images'):
         self.log_items = []
-        self.first_iteration = True
-        self.get_drone_action = get_drone_action
+        self.drone = drone
         self.name = name
         self.image_dir = image_dir
 
@@ -18,10 +19,10 @@ class DroneControlTab(BCIGuiTab):
         return self.name
 
     def add_item_to_log(self, item, window=None):
-        self.log_items.insert(0, item)
+        self.log_items.append(item)
         if window:
             log = window[self.key('LOG')]
-            window[self.key('LOG')].update(values=self.log_items)
+            window[self.key('LOG')].update(value='\n'.join(self.log_items))
             window.refresh()
 
     def get_tab(self):
@@ -31,8 +32,9 @@ class DroneControlTab(BCIGuiTab):
 
         top_center = [
             [sg.Button('Up', size=(8, 2), expand_x=True, expand_y=True, image_filename=f'{self.image_dir}/up.png')]]
-        top_right = [[sg.Text('Flight Log')], [sg.Listbox(
-            values=[], size=(40, 5), key=self.key('LOG'))]]
+        top_right = [[sg.Text('Flight Log')],
+                     [sg.Multiline(default_text='---------- NEW LOG ----------', auto_refresh=True,
+                                   autoscroll=True, write_only=True, size=(40, 5), key=self.key('LOG'))]]
         bottom_center = [
             [sg.Button('Down', size=(8, 2), expand_x=True, expand_y=True, image_filename=f'{self.image_dir}/down.png')]]
 
@@ -43,13 +45,15 @@ class DroneControlTab(BCIGuiTab):
             [sg.Button('Forward', size=(8, 2), expand_x=True, expand_y=True,
                        image_filename=f'{self.image_dir}/forward.png')],
 
-            [sg.Button('Turn Left', size=(8, 2), expand_x=True, expand_y=True, image_filename=f'{self.image_dir}/turnLeft.png'),
+            [sg.Button('Turn Left', size=(8, 2), expand_x=True, expand_y=True,
+                       image_filename=f'{self.image_dir}/turnLeft.png'),
              sg.Button('Left', size=(8, 2), expand_x=True, expand_y=True,
                        image_filename=f'{self.image_dir}/left.png'),
              sg.Button('Stream', expand_x=True, expand_y=True, image_filename=f'{self.image_dir}/drone.png'),
              sg.Button('Right', size=(8, 2), expand_x=True, expand_y=True,
                        image_filename=f'{self.image_dir}/right.png'),
-             sg.Button('Turn Right', size=(8, 2), expand_x=True, expand_y=True, image_filename=f'{self.image_dir}/turnRight.png')],
+             sg.Button('Turn Right', size=(8, 2), expand_x=True, expand_y=True,
+                       image_filename=f'{self.image_dir}/turnRight.png')],
 
             [sg.Button('Back', size=(8, 2), expand_x=True, expand_y=True,
                        image_filename=f'{self.image_dir}/back.png')],
@@ -63,56 +67,51 @@ class DroneControlTab(BCIGuiTab):
         return tab
 
     def handle_event(self, window, event, values):
-        if self.first_iteration:
-            self.add_item_to_log("---------- NEW LOG ----------", window=window)
-            self.first_iteration = False
-
         if event == 'Up':
-            self.get_drone_action('up')
-            self.add_item_to_log("Up button pressed", window=window)
+            self.drone.send_action('up')
+            self.add_item_to_log('Up button pressed', window=window)
             self.add_item_to_log('done', window=window)
         elif event == 'Down':
-            self.add_item_to_log("Down button pressed", window=window)
-            self.get_drone_action('down')
+            self.add_item_to_log('Down button pressed', window=window)
+            self.drone.send_action('down')
             self.add_item_to_log('done', window=window)
         elif event == 'Forward':
-            self.add_item_to_log("Forward button pressed", window=window)
-            self.get_drone_action('forward')
+            self.add_item_to_log('Forward button pressed', window=window)
+            self.drone.send_action('forward')
             self.add_item_to_log('done', window=window)
         elif event == 'Back':
-            self.add_item_to_log("Back button pressed", window=window)
-            self.get_drone_action('backward')
+            self.add_item_to_log('Back button pressed', window=window)
+            self.drone.send_action('backward')
             self.add_item_to_log('done', window=window)
         elif event == 'Left':
-            self.add_item_to_log("Left button pressed", window=window)
-            self.get_drone_action('left')
+            self.add_item_to_log('Left button pressed', window=window)
+            self.drone.send_action('left')
             self.add_item_to_log('done', window=window)
         elif event == 'Right':
-            self.add_item_to_log("Right button pressed", window=window)
-            self.get_drone_action('right')
+            self.add_item_to_log('Right button pressed', window=window)
+            self.drone.send_action('right')
             self.add_item_to_log('done', window=window)
         elif event == 'Turn Left':
-            self.add_item_to_log("Turn Left button pressed", window=window)
-            self.get_drone_action('turn_left')
+            self.add_item_to_log('Turn Left button pressed', window=window)
+            self.drone.send_action('turn_left')
             self.add_item_to_log('done', window=window)
         elif event == 'Turn Right':
-            self.add_item_to_log("Turn right button pressed", window=window)
-            self.get_drone_action('turn_right')
+            self.add_item_to_log('Turn right button pressed', window=window)
+            self.drone.send_action('turn_right')
             self.add_item_to_log('done', window=window)
         elif event == 'Takeoff':
-            self.add_item_to_log("Takeoff button pressed", window=window)
-            self.get_drone_action('takeoff')
-            self.add_item_to_log("Done.", window=window)
+            self.add_item_to_log('Takeoff button pressed', window=window)
+            self.drone.send_action('takeoff')
+            self.add_item_to_log('Done.', window=window)
         elif event == 'Land':
-            self.add_item_to_log("Land button pressed", window=window)
-            self.get_drone_action('land')
+            self.add_item_to_log('Land button pressed', window=window)
+            self.drone.send_action('land')
             self.add_item_to_log('done', window=window)
         elif event == 'Home':
-            self.first_iteration = True
+            print('no')
         elif event == 'Connect1':
-            self.add_item_to_log("Connect button pressed", window=window)
-            self.get_drone_action('connect')
-            self.add_item_to_log("Done.", window=window)
+            self.add_item_to_log('Connect button pressed', window=window)
+            self.drone.send_action('connect')
+            self.add_item_to_log('Done.', window=window)
         elif event == 'Stream':
             print('no')
-            # self.get_drone_action('stream')
